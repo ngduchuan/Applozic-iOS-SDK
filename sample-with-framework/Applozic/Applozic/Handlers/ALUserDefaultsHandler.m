28a06a67564ffa680fc0313f3f2783374b8651f0
Sunilkumarr <sunil@applozic.com>
2020-12-30 16:15:22 +0530

    Keep the VOIP device token from removing from user defaults

--- sample-with-framework/Applozic/Applozic/Handlers/ALUserDefaultsHandler.m
+++ sample-with-framework/Applozic/Applozic/Handlers/ALUserDefaultsHandler.m
@@ -97,8 +97,9 @@
     NSArray * keyArray = [dictionary allKeys];
     for(NSString * defaultKeyString in keyArray)
     {
-        if([defaultKeyString hasPrefix:AL_KEY_PREFIX] && ![defaultKeyString isEqualToString:AL_APN_DEVICE_TOKEN])
-        {
+        if([defaultKeyString hasPrefix:AL_KEY_PREFIX] &&
+           ![defaultKeyString isEqualToString:AL_APN_DEVICE_TOKEN] &&
+           ![defaultKeyString isEqualToString:AL_VOIP_DEVICE_TOKEN]) {
             [userDefaults removeObjectForKey:defaultKeyString];
             [userDefaults synchronize];
         }
