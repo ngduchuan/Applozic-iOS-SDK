f4435c3c4c69ab5dad8cfe425a30f5bc0a3a4c30
Sunilkumarr <sunil@applozic.com>
2021-03-08 17:13:15 +0530

    Add the ContinerListViewController for adding messagelist vc

---
+++ sample-with-framework/applozicdemo/ContinerListViewController.h
@@ -0,0 +1,17 @@
+//
+//  ContinerListViewController.h
+//  applozicdemo
+//
+//  Created by apple on 08/03/21.
+//  Copyright © 2021 applozic Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface ContinerListViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
